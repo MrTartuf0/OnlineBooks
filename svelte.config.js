9c2eb107b6beb8001affcd09e1388a4894ae4289
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-08-18 23:11:53 +0200

    netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/OnlineBooks' : '',
-		}
+		adapter: adapter({
+			edge: false,
+      split: false
+		}),
 	},
 	preprocess: vitePreprocess(),
 };
