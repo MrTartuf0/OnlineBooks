3e1e2700fcb197f645fc5dc3efaeff82b3ac7e80
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-08-18 23:41:53 +0200

    build

---
+++ .netlify/server/manifest-full.js
@@ -0,0 +1,34 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.b5e575ee.js","app":"_app/immutable/entry/app.1c01339c.js","imports":["_app/immutable/entry/start.b5e575ee.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.6ce8577b.js","_app/immutable/entry/app.1c01339c.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})();
---
+++ .netlify/server/nodes/0.js
@@ -0,0 +1,8 @@
+
+
+export const index = 0;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/0.2dd57823.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js"];
+export const stylesheets = ["_app/immutable/assets/0.c2d9b0c4.css"];
+export const fonts = [];
